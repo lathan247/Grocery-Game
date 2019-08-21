2e3f69c4f364832c41f56734e8af78fac8f97b62
lathan247 <51328330+lathan247@users.noreply.github.com>
2019-08-21 10:31:13 -0400

    working with arrays

---
+++ arr.js
@@ -0,0 +1,24 @@
+"use strict"
+
+// var groceryList = [
+//     "bread", "milk", "eggs", "water", "cereal", "chips", "icecream", "seasoning", "steak", "chicken" ];
+//     for ( var i = 0; i < groceryList.length; i++) {
+//         console.log(groceryList[i]);
+//     }
+
+    // var groceryList = "steaks and wings";
+    // for (var i = 0; i < groceryList.length; i++){
+    //     console.log(groceryList[i]);
+    // }
+    // groceryList = groceryList.toUpperCase();
+    // console.log(groceryList);
+
+// [["tomato", 5], ["guavas", 7]]
+// groceryList[i][0], groceryList[i][1]
+
+    
+    var groceryStore = [["tomato",4], ["onions",6], ["lettuce",3]];
+     for(var i = 0 ; i <= groceryStore.length ; i);                                                                                                                                                                                                   
+    console.log(i, groceryStore[0]);
+    var groceryCart = prompt("Choose your grocery item here");
+
